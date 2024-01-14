d810fe0faccefdaf6e7e5bc960a3e35d5e8fdecd
alan890104 <alan890104@gmail.com>
2024-01-14 12:54:29 +0800

    Add warning for missing API key

---
+++ README.md
@@ -0,0 +1,10 @@
+# Pytoncenter
+
+## Installation
+```bash
+pip3 install pytoncenter
+```
+
+## Examples
+1. [Execute Parallelly](./examples/execute_many.py)
+2. [Subscribe transactions for address](./examples/subscribe.py)
--- pytoncenter/api.py
+++ pytoncenter/api.py
@@ -4,6 +4,7 @@ from typing import Dict, List, Optional, Union, Literal, Tuple, TypedDict, Corou
 import aiohttp
 import asyncio
 from .types import *
+import warnings
 
 class TonException(Exception):
     def __init__(self, code: int):
@@ -12,7 +13,8 @@ class TonException(Exception):
 class AsyncTonCenterClient:
     def __init__(self, network: Union[Literal["mainnet"], Literal["testnet"]], *, custom_api_key: Optional[str]=None, custom_base_url: Optional[str]=None) -> None:
         api_key = os.getenv("TONCENTER_API_KEY", custom_api_key)
-        assert api_key, "API key must be provided in environment variable `TONCENTER_API_KEY` or custom_api_key"
+        # show warning if api_key is None
+        warnings.warn("API key is not provided. TonCenter API is rate limited to 1 requests per second. Suggesting providing it in environment variable `TONCENTER_API_KEY` or custom_api_key to increase the rate limit.", RuntimeWarning)        
         assert (network in ["mainnet", "testnet"]) or (custom_base_url is not None), "Network or custom_base_url must be provided"
         if custom_base_url is not None:
             self.base_url = custom_base_url
