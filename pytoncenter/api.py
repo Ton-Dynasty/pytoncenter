591a0bbef06dac1aad98bd6bd66928907a8e5aa0
alan890104 <alan890104@gmail.com>
2024-01-14 13:06:30 +0800

    Add warning for missing API key in TonCenter API initialization

--- pytoncenter/api.py
+++ pytoncenter/api.py
@@ -14,7 +14,8 @@ class AsyncTonCenterClient:
     def __init__(self, network: Union[Literal["mainnet"], Literal["testnet"]], *, custom_api_key: Optional[str]=None, custom_base_url: Optional[str]=None) -> None:
         api_key = os.getenv("TONCENTER_API_KEY", custom_api_key)
         # show warning if api_key is None
-        warnings.warn("API key is not provided. TonCenter API is rate limited to 1 requests per second. Suggesting providing it in environment variable `TONCENTER_API_KEY` or custom_api_key to increase the rate limit.", RuntimeWarning)        
+        if not api_key:
+            warnings.warn("API key is not provided. TonCenter API is rate limited to 1 request per second. Suggesting providing it in environment variable `TONCENTER_API_KEY` or custom_api_key to increase the rate limit.", RuntimeWarning)        
         assert (network in ["mainnet", "testnet"]) or (custom_base_url is not None), "Network or custom_base_url must be provided"
         if custom_base_url is not None:
             self.base_url = custom_base_url
