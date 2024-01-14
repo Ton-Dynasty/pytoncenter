3ea5fcbe6a4fb2d5c0bd7aa62621d175d33e3c2b
ipromise2324 <168allen55@gmail.com>
2024-01-15 01:04:15 +0800

    feat: add Address class and address example

--- pytoncenter/address.py
+++ pytoncenter/address.py
@@ -1,30 +1,170 @@
+from .utils import crc16, string_to_bytes
+import base64
+import ctypes
+
+
+def parse_friendly_address(addr_str):
+    if len(addr_str) != 48:
+        raise Exception("User-friendly address should contain strictly 48 characters")
+
+    # avoid padding error (https://gist.github.com/perrygeo/ee7c65bb1541ff6ac770)
+    data = string_to_bytes(base64.b64decode(addr_str + "=="))
+
+    if len(data) != 36:
+        raise Exception("Unknown address type: byte length is not equal to 36")
+
+    addr = data[:34]
+    crc = data[34:36]
+    calced_crc = crc16(addr)
+    if not (calced_crc[0] == crc[0] and calced_crc[1] == crc[1]):
+        raise Exception("Wrong crc16 hashsum")
+
+    tag = addr[0]
+    is_test_only = False
+    is_bounceable = False
+    if tag & Address.TEST_FLAG:
+        is_test_only = True
+        tag ^= Address.TEST_FLAG
+    if (tag != Address.BOUNCEABLE_TAG) and (tag != Address.NON_BOUNCEABLE_TAG):
+        raise Exception("Unknown address tag")
+
+    is_bounceable = tag == Address.BOUNCEABLE_TAG
+
+    if addr[1] == 0xFF:
+        workchain = -1
+    else:
+        workchain = addr[1]
+    if workchain != 0 and workchain != -1:
+        raise Exception(f"Invalid address wc {workchain}")
+
+    hash_part = bytearray(addr[2:34])
+    return {
+        "is_test_only": is_test_only,
+        "is_bounceable": is_bounceable,
+        "workchain": workchain,
+        "hash_part": hash_part,
+    }
+
+
 class Address:
-    def __init__(self, any_form_addr: str):
-        pass
-    
+    BOUNCEABLE_TAG = 0x11
+    NON_BOUNCEABLE_TAG = 0x51
+    TEST_FLAG = 0x80
+
+    def __init__(self, any_form: str):
+        if any_form is None:
+            raise Exception("Invalid address")
+
+        if isinstance(any_form, Address):
+            self._wc = any_form._wc
+            self._hash_part = any_form._hash_part
+            self._is_test_only = any_form._is_test_only
+            self._is_user_friendly = any_form._is_user_friendly
+            self._is_bounceable = any_form._is_bounceable
+            self._is_url_safe = any_form._is_url_safe
+            return
+
+        # base64 with digits, upper and lowercase Latin letters, '/' and '+'
+        # base64url with '_' and '-' instead of '/' and '+')
+        if any_form.find("-") > 0 or any_form.find("_") > 0:
+            any_form = any_form.replace("-", "+").replace("_", "/")
+            self._is_url_safe = True
+        else:
+            self._is_url_safe = False
+
+        # Check this address is raw address or not
+        try:
+            colon_index = any_form.index(":")
+        except ValueError:
+            colon_index = -1  # raw address
+
+        if colon_index > -1:
+            # Example of Raw Address, -1:fcb91a3a3816d0f7b8c2c76108b8a9bc5a6b7a55bd79f8ab101c52db29232260
+            arr = any_form.split(":")
+            if len(arr) != 2:
+                raise Exception(f"Invalid address {any_form}")
+
+            wc = int(arr[0])
+            if wc != 0 and wc != -1:
+                raise Exception(f"Invalid address wc {wc}")
+
+            address_hex = arr[1]
+            if len(address_hex) != 64:
+                raise Exception(f"Invalid address hex {any_form}")
+
+            self._is_user_friendly = False
+            self._wc = wc
+            self._hash_part = bytearray.fromhex(address_hex)
+            self._is_test_only = False
+            self._is_bounceable = False
+        else:
+            self._is_user_friendly = True
+            parse_result = parse_friendly_address(any_form)
+            self._wc = parse_result["workchain"]
+            self._hash_part = parse_result["hash_part"]
+            self._is_test_only = parse_result["is_test_only"]
+            self._is_bounceable = parse_result["is_bounceable"]
+
     @property
     def workchain(self) -> int:
-        return 0
-    
+        return self._wc
+
     @property
     def hash_part(self) -> bytearray:
-        return ""
-    
+        return self._hash_part
+
     @property
     def is_test_only(self) -> bool:
-        return False
-    
+        return self._is_test_only
+
     @property
     def is_user_friendly(self) -> bool:
-        return False
-    
+        return self._is_user_friendly
+
     @property
     def is_bounceable(self) -> bool:
-        return False
-    
+        return self._is_bounceable
+
     @property
     def is_url_safe(self) -> bool:
-        return False
-    
-    def to_string(self) -> str:
-        return ""
+        return self._is_url_safe
+
+    def to_string(
+        self,
+        is_user_friendly=None,
+        is_url_safe=None,
+        is_bounceable=None,
+        is_test_only=None,
+    ) -> str:
+        if is_user_friendly is None:
+            is_user_friendly = self._is_user_friendly
+        if is_url_safe is None:
+            is_url_safe = self._is_url_safe
+        if is_bounceable is None:
+            is_bounceable = self._is_bounceable
+        if is_test_only is None:
+            is_test_only = self._is_test_only
+
+        if not is_user_friendly:
+            return f"{self._wc}:{self._hash_part.hex()}"
+        else:
+            tag = (
+                Address.BOUNCEABLE_TAG if is_bounceable else Address.NON_BOUNCEABLE_TAG
+            )
+
+            if is_test_only:
+                tag |= Address.TEST_FLAG
+
+            addr = (ctypes.c_int8 * 34)()
+            addr[0] = tag
+            addr[1] = self._wc
+            addr[2:] = self._hash_part
+            address_with_checksum = (ctypes.c_uint8 * 36)()
+            address_with_checksum[:34] = addr
+            address_with_checksum[34:] = crc16(addr)
+
+            address_base_64 = base64.b64encode(address_with_checksum).decode("utf-8")
+            if is_url_safe:
+                address_base_64 = address_base_64.replace("+", "-").replace("/", "_")
+
+            return str(address_base_64)
--- pytoncenter/contract.py
+++ pytoncenter/contract.py
@@ -4,11 +4,13 @@ from abc import ABC
 from typing import Optional, Dict, TypedDict, Union
 from .address import Address
 
+
 class StateInit(TypedDict):
     code: Cell
     data: Cell
     address: Address
     state_init: Cell
+
 
 class ExternalMessage(TypedDict):
     address: Address
@@ -16,6 +18,7 @@ class ExternalMessage(TypedDict):
     state_init: Cell
     code: Cell
     data: Cell
+
 
 class ExternalMessageWithSignature(TypedDict):
     address: Address
@@ -35,32 +38,37 @@ class SendMode(Enum):
     SendDestroyIfZero = 32
     SendRemainingValue = 64
     SendRemainingBalance = 128
-    
+
+
 class Contract(ABC):
-    def __init__(self, address:Optional[str]=None, workchain: Optional[int]=None, **options):
+    def __init__(
+        self, address: Optional[str] = None, workchain: Optional[int] = None, **options
+    ):
         self._address = address
         self._wc = workchain
         self.options = options
-    
+
     @property
     def address(self):
         if self._address is None:
             self._address = self.create_state_init()["address"]
-            
+
     def get_code_cell(self):
         code = self.options.get("code")
         assert isinstance(code, Cell), "Contract: code must be Cell"
         if not self.options.get("code"):
             raise Exception("Contract: code is not set in options")
         return code
-    
+
     def get_data_cell(self) -> Cell:
         return Cell()
-    
+
     def create_init_external_message(self) -> ExternalMessage:
         init = self.create_state_init()
         header = Contract.create_external_message_header(init["address"])
-        external_msg = Contract.create_common_msg_info(header=header, state_init=init["state_init"])
+        external_msg = Contract.create_common_msg_info(
+            header=header, state_init=init["state_init"]
+        )
         return {
             "address": init["address"],
             "message": external_msg,
@@ -68,9 +76,11 @@ class Contract(ABC):
             "code": init["code"],
             "data": init["data"],
         }
-        
+
     @classmethod
-    def create_external_message_header(cls: "Contract", dest: str, src:Optional[str]=None, import_fee:int = 0) -> Cell:
+    def create_external_message_header(
+        cls: "Contract", dest: str, src: Optional[str] = None, import_fee: int = 0
+    ) -> Cell:
         cell = begin_cell()
         cell.store_uint(2, 2)
         if src is None:
@@ -80,16 +90,29 @@ class Contract(ABC):
         cell.store_address(dest)
         cell.store_grams(import_fee)
         return cell
-    
+
     @classmethod
-    def create_internal_message_header(cls: "Contract", dest: str, coins: int = 0, ihr_disabled:bool=True, bounce: Optional[bool]=None, bounced: bool=False, src:Optional[str]=None, currency_collection: Optional[bool]=None, ihr_fees: int = 0, fwd_fees:int=0, created_lt:int = 0, created_at: int = 0):
+    def create_internal_message_header(
+        cls: "Contract",
+        dest: str,
+        coins: int = 0,
+        ihr_disabled: bool = True,
+        bounce: Optional[bool] = None,
+        bounced: bool = False,
+        src: Optional[str] = None,
+        currency_collection: Optional[bool] = None,
+        ihr_fees: int = 0,
+        fwd_fees: int = 0,
+        created_lt: int = 0,
+        created_at: int = 0,
+    ):
         message = begin_cell()
         message.store_bool(False)
         message.store_bool(ihr_disabled)
         if bounce is not None:
             message.store_bool(bounce)
         else:
-            message.store_bool(Address(dest).is_bounceable)
+            message.store_bool(Address(dest)._is_bounceable)
         message.store_bool(bounced)
         if src is None:
             message.store_uint(0, 2)
@@ -105,9 +128,14 @@ class Contract(ABC):
         message.store_uint(created_lt, 64)
         message.store_uint(created_at, 32)
         return message
-    
+
     @classmethod
-    def create_common_msg_info(cls: "Contract", header: Cell, state_init: Optional[Cell]=None, body: Optional[Cell]=None) -> Cell:
+    def create_common_msg_info(
+        cls: "Contract",
+        header: Cell,
+        state_init: Optional[Cell] = None,
+        body: Optional[Cell] = None,
+    ) -> Cell:
         common_msg_info = begin_cell()
         common_msg_info.store_builder(header)
         if state_init:
@@ -115,15 +143,15 @@ class Contract(ABC):
             # TODO: get free bits
         else:
             common_msg_info.store_bool(False)
-            
+
         # Body part
         if body:
             pass
         else:
             common_msg_info.store_bool(False)
-        
+
         return common_msg_info
-            
+
     def create_state_init(self) -> StateInit:
         code_cell = self.get_code_cell()
         data_cell = self.get_data_cell()
@@ -131,52 +159,66 @@ class Contract(ABC):
         split_depth = None
         tick_tock = None
         library = None
-        state_init.store_bitstring("".join([
-            int(bool(split_depth)),
-            int(bool(tick_tock)),
-            int(code_cell),
-            int(data_cell),
-            int(bool(library))
-        ]))
+        state_init.store_bitstring(
+            "".join(
+                [
+                    int(bool(split_depth)),
+                    int(bool(tick_tock)),
+                    int(code_cell),
+                    int(data_cell),
+                    int(bool(library)),
+                ]
+            )
+        )
         return {
             "code": code_cell,
             "data": data_cell,
             "address": Address(f"{self._wc}:{state_init.get_hash()}"),
             "state_init": state_init.end_cell(),
         }
-        
+
 
 class WalletContract(Contract):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        
+
     @staticmethod
     def from_keypair(public_key: str, private_key: str, **kwargs):
         kwargs["public_key"] = public_key
         kwargs["private_key"] = private_key
         return WalletContract(**kwargs)
-    
+
     @staticmethod
     def from_address(address: str, **kwargs):
         kwargs["address"] = address
         return WalletContract(**kwargs)
-    
+
     def get_data_cell(self) -> Cell:
         cell = begin_cell()
         cell.store_uint(0, 32)
-    
+
     def create_signing_message(self, seqno: Optional[int] = None):
         pass
-    
-    def create_transfer_message(self, to_addr: str, amount: int, seqno: int, payload: Union[Cell, str, bytes, None] = None, send_mode: SendMode = SendMode.SendIgnoreErrors | SendMode.SendPayGasSeparately, dummy_signature:bool = False, state_init: Optional[Cell] = None):
+
+    def create_transfer_message(
+        self,
+        to_addr: str,
+        amount: int,
+        seqno: int,
+        payload: Union[Cell, str, bytes, None] = None,
+        send_mode: SendMode = SendMode.SendIgnoreErrors | SendMode.SendPayGasSeparately,
+        dummy_signature: bool = False,
+        state_init: Optional[Cell] = None,
+    ):
         return
-    
-    def create_external_message(self, signing_message: Cell, seqno: int, dummy_signature: bool = False) -> ExternalMessageWithSignature:
+
+    def create_external_message(
+        self, signing_message: Cell, seqno: int, dummy_signature: bool = False
+    ) -> ExternalMessageWithSignature:
         pass
-    
+
     def create_init_external_message(self) -> ExternalMessage:
         return super().create_init_external_message()
-
 
 
 class WalletContractV4(WalletContract):
