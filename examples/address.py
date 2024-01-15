d4ed853c1c7de9d79e3ef0d4b4718ed3e25e949b
ipromise2324 <168allen55@gmail.com>
2024-01-15 12:04:34 +0800

    feat: add more address examples

--- examples/address.py
+++ examples/address.py
@@ -8,7 +8,12 @@ async def main():
         "0:2e56d1faf0f46433caac31487aef479c3593edaace5b00d177303e1dbfe7816f"
     )
     friendly_address_str = "EQAuVtH68PRkM8qsMUh670ecNZPtqs5bANF3MD4dv-eBb2vD"
-    test_address_str = friendly_address_str
+    bounceable_address_str = "EQAuVtH68PRkM8qsMUh670ecNZPtqs5bANF3MD4dv+eBb2vD"
+    bounceable_address_str_url = "EQAuVtH68PRkM8qsMUh670ecNZPtqs5bANF3MD4dv-eBb2vD"
+    non_bounceable_address_str = "UQAuVtH68PRkM8qsMUh670ecNZPtqs5bANF3MD4dv+eBbzYG"
+    non_bounceable_address_str_url = "UQAuVtH68PRkM8qsMUh670ecNZPtqs5bANF3MD4dv-eBbzYG"
+
+    test_address_str = bounceable_address_str_url
     addr = Address(test_address_str)
 
     client = AsyncTonCenterClient(network="mainnet")
@@ -29,28 +34,40 @@ async def main():
     print(f"Is User Friendly: {addr.is_user_friendly}")
     print(f"Is Bounceable: {addr.is_bounceable}")
     print(f"Is URL Safe: {addr.is_url_safe}")
-
     address_string = addr.to_string()
     print(f"Address String: {address_string}")
 
     assert result["test_only"] == addr.is_test_only, "Test only mismatch!"
+
     assert result["raw_form"][2:] == addr.hash_part.hex(), "User friendly mismatch!"
 
     if result["given_type"] == "friendly_bounceable":
         assert addr.is_user_friendly == True, "User friendly mismatch!"
         assert addr.is_bounceable == True, "Bounceable mismatch!"
 
+    if result["given_type"] == "friendly_non_bounceable":
+        assert addr.is_user_friendly == True, "User friendly mismatch!"
+        assert addr.is_bounceable == False, "Bounceable mismatch!"
+
     if result["given_type"] == "raw_form":
         assert addr.is_user_friendly == False, "User friendly mismatch!"
         assert addr.is_bounceable == False, "Bounceable mismatch!"
 
     if (
-        test_address_str == result["bounceable"]["b64"]
-        or test_address_str == result["bounceable"]["b64url"]
-        or test_address_str == result["bounceable"]["b64"]
+        test_address_str == result["non_bounceable"]["b64url"]
         or test_address_str == result["bounceable"]["b64url"]
     ):
         assert addr.is_url_safe == True, "URL safe mismatch!"
+    else:
+        assert addr.is_url_safe == False, "URL safe mismatch!"
+
+    assert (
+        address_string == result["bounceable"]["b64"]
+        or address_string == result["bounceable"]["b64url"]
+        or address_string == result["non_bounceable"]["b64"]
+        or address_string == result["non_bounceable"]["b64url"]
+        or address_string == result["raw_form"]
+    ), "Address string mismatch!"
 
 
 if __name__ == "__main__":
