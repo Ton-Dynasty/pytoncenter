8d662ea984c5dcfb115fea788b5ac37cce8f6262
alan890104 <alan890104@gmail.com>
2024-02-17 23:27:59 +0800

    perf: enhance 2x speed for getting trace

--- examples/v3/get_prev_tx.py
+++ examples/v3/get_prev_tx.py
@@ -1,4 +1,5 @@
 import asyncio
+import time
 
 from pytoncenter import get_client
 from pytoncenter.v3.models import *
@@ -14,11 +15,31 @@ https://testnet.tonviewer.com/transaction/f5333b49b754376ff96b0d299dc38142b8b5ca
 
 async def main():
     client = get_client(version="v3", network="testnet")
+
+    print("==============Reommanded way (Fast)=====================")
+
+    start = time.monotonic()
+    txs = await client.get_adjacent_transactions(
+        GetAdjacentTransactionsRequest(
+            hash="84b7c9467a0a24e7a59a5e224e9ef8803563621f4710fe8536ae7803fe245d61",
+            direction="in",
+        )
+    )
+    for _tx in txs:
+        print("Prev Transaction hash", _tx.hash)
+    end = time.monotonic()
+    print("Time elapsed:", end - start)
+
+    print("==============Alternative way (Slow)=====================")
+
+    start = time.monotonic()
     tx = await client.get_transactions(GetTransactionByHashRequest(hash="84b7c9467a0a24e7a59a5e224e9ef8803563621f4710fe8536ae7803fe245d61"))
     assert tx is not None
     txs = await client.get_transaction_by_message(GetTransactionByMessageRequest(direction="out", msg_hash=tx.in_msg.hash))
     for _tx in txs:
         print("Prev Transaction hash", _tx.hash)
+    end = time.monotonic()
+    print("Time elapsed:", end - start)
 
 
 if __name__ == "__main__":
