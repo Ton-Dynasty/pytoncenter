f91331b5cfbf575488765b726ddcf99608d83389
Yu Lun Hsu <alan890104@gmail.com>
2024-02-18 18:38:48 +0800

    Update wait_message_exists.py

--- examples/v3/wait_message_exists.py
+++ examples/v3/wait_message_exists.py
@@ -4,17 +4,14 @@ from pytoncenter import get_client
 from pytoncenter.utils import format_trace
 from pytoncenter.v3.models import *
 
-"""
-Take this transaction as example:
-https://testnet.tonviewer.com/transaction/84b7c9467a0a24e7a59a5e224e9ef8803563621f4710fe8536ae7803fe245d61
-
-The output transactions should be:
-https://testnet.tonviewer.com/transaction/f5333b49b754376ff96b0d299dc38142b8b5ca1b046f41704a4814b51700c89d
-"""
-
 
 async def main():
     client = get_client(version="v3", network="testnet")
+    
+    # sent_message = await client.send_message(ExternalMessage())
+    # msg_hash = sent_message.message_hash
+    # Take msg_hash = "82CBB53717ACE80D7A1263243F7CA4085C6C0B65A05ABBC41C7BA38D435507DF" as example
+
     generator = client.wait_message_exists(
         WaitMessageExistsRequest(
             msg_hash="82CBB53717ACE80D7A1263243F7CA4085C6C0B65A05ABBC41C7BA38D435507DF",
