8e2fe3bdcf90e825eeeead0f3339e3fa9107e26c
alan890104 <alan890104@gmail.com>
2024-02-20 04:05:52 +0800

    test: remove duplicate test

--- tests/v3/transactions.py
+++ tests/v3/transactions.py
@@ -1,8 +1,6 @@
 import pytest
 
 from pytoncenter import AsyncTonCenterClientV3, get_client
-from pytoncenter.address import Address
-from pytoncenter.utils import create_address_mapping, format_trace
 from pytoncenter.v3.models import *
 
 pytest_plugins = ("pytest_asyncio",)
@@ -24,21 +22,6 @@ class TestDecoder:
         assert not isinstance(state, Exception)
         shards = client.get_masterchain_block_shards(GetMasterchainBlockShardsRequest(seqno=10, include_mc_block=True))
         assert not isinstance(shards, Exception)
-
-    @pytest.mark.asyncio
-    async def test_get_trace_alternative(self):
-        trace = await self.client.get_trace_alternative(GetTransactionTraceRequest(hash="84b7c9467a0a24e7a59a5e224e9ef8803563621f4710fe8536ae7803fe245d61", sort="asc"))
-        addr_mapping = create_address_mapping(
-            {
-                Address("0QApdUMEOUuHnBo-RSdbikkZZ3qWItZLdXjyff9lN_eS5Zib"): "Alan Wallet V4",
-                Address("kQCQ1B7B7-CrvxjsqgYT90s7weLV-IJB2w08DBslDdrIXucv"): "Alan USD Jetton Wallet",
-                Address("kQDO_0Z0SuVpqpaNE0dPxUiFCNDpdR4ODW9KQAwgQGwc5wiB"): "Oracle Jetton Wallet",
-                Address("kQCpk40ub48fvx89vSUjOTRy0vOEEZ4crOPPfLEvg88q1EeH"): "Oracle",
-                Address("kQA0FY6YIacA0MgDlKN_qMQuXVZqL3qStyyaNkVB-svHQqsJ"): "New Alarm",
-            }
-        )
-        output = format_trace(trace, address_mapping=addr_mapping)
-        assert output
 
     @pytest.mark.asyncio
     async def test_get_address_book(self):
